89002d01046fe3555284dd090fbb349b3dbdb3cd
isaaccavallaro <isaaccavallaro@gmail.com>
2023-02-03 20:13:45 +1000

    Adding logs

---
+++ cdk.out/asset.6ba4ec35f9d6b61f727d1bd0fb5bc5cc8b02e94a4f5e6e415b7f8fb705a9b576/index.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    
+    const tableName = "serverless-todo-stack-todostableCA1914CD-Z8KD3B8A4HVN";
+
+    if (event.httpMethod === 'GET') {
+        // Retrieve all items from the table
+        const result = await dynamo.scan({ TableName: tableName }).promise();
+        const items = result.Items;
+
+        // Return the items as a response
+        return {
+            statusCode: 200,
+            body: JSON.stringify(items),
+        };
+    } else if (event.httpMethod === 'POST') {
+        // Extract the to-do item from the request body
+        const item = JSON.parse(event.body).item;
+
+        // Generate a unique ID for the item
+        const id = Date.now().toString();
+
+        // Insert the item into the table
+        await dynamo.put({
+            TableName: tableName,
+            Item: {
+                id: id,
+                item: item
+            }
+        }).promise();
+
+        // Return the inserted item as a response
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                id: id,
+                item: item
+            })
+        };
+    } else {
+        // Return an error for unsupported HTTP methods
+        return {
+            statusCode: 400,
+            body: 'Unsupported HTTP method'
+        };
+    }
+};
--- cdk/lib/todo-stack.ts
+++ cdk/lib/todo-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as iam from 'aws-cdk-lib/aws-iam';
-
+import * as logs from 'aws-cdk-lib/aws-logs';
 
 export class TodoStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -11,30 +11,44 @@ export class TodoStack extends cdk.Stack {
         // DynamoDB Table
         const table = new dynamodb.Table(this, 'todos-table', {
             partitionKey: { name: 'id', type: dynamodb.AttributeType.NUMBER },
-            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            billingMode: dynamodb.BillingMode.PROVISIONED,
+            readCapacity: 5,
+            writeCapacity: 5,
+            removalPolicy: cdk.RemovalPolicy.RETAIN,
+            tableName: 'todos-table',
         });
 
         new cdk.CfnOutput(this, 'tableName', { value: table.tableName });
 
         const role = new iam.Role(this, 'DynamoDBFunctionRole', {
-          assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-          managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')],
+            assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+            managedPolicies: [iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess')],
         });
-        
+
         const todoFunction = new lambda.Function(this, 'todoFunction', {
-          runtime: lambda.Runtime.NODEJS_16_X,
-          code: lambda.Code.fromAsset('lambda'),
-          handler: 'index.handler',
-          environment: {
-            TABLE_NAME: table.tableName
-          },
-          role: role
+            runtime: lambda.Runtime.NODEJS_16_X,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'index.handler',
+            environment: {
+                TABLE_NAME: table.tableName
+            },
+            role: role,
+            logRetention: logs.RetentionDays.ONE_WEEK,
         });
 
         // Give the Lambda function permissions to access the DynamoDB table
         table.grantReadWriteData(todoFunction);
 
         new cdk.CfnOutput(this, 'functionArn', { value: todoFunction.functionArn });
+
+        const logGroup = new logs.LogGroup(this, 'LogGroup', {
+            retention: logs.RetentionDays.ONE_WEEK,
+            logGroupName: 'serverless-todo-logs'
+        });
+
+        todoFunction.addToRolePolicy(new iam.PolicyStatement({
+            actions: ['logs:CreateLogStream', 'logs:PutLogEvents'],
+            resources: [logGroup.logGroupArn]
+        }));
     }
 }
